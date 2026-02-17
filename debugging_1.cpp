5cee710b89f517e93b06d10b82e2cc604131c4d1
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-02-17 14:13:31 +0530

    Add files via upload

---
+++ debugging_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter number of terms:";
+    cin >> n;
+    if (n<=0){
+        cout << "Invalid output";
+        return 0;
+    }
+    int a = 0 , b= 1 ,c; 
+
+    cout << a << " " << b << " ";
+    for (int i = 2 ;  i<n ; i++){
+        c = a+b;
+        cout << c << " ";
+        a = b;
+        b = c;
+    }
+    return 0;
+}
---
+++ debugging_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5] = {1, 2, 2, 3, 1};
+    for (int i = 0; i < 5; i++) {
+        int count = 0;
+        int flag =0;
+        for (int k = 0; k < i; k++) {
+            if (arr[k] == arr[i]) {
+                flag=1;
+                break;
+            }
+        }
+        if (flag==1) {
+            continue;
+        }
+        for (int j = 0; j < 5; j++) {
+            if (arr[i] == arr[j])
+                count++;
+        }
+        cout << arr[i] << " occurs " << count << " times\n";
+    }
+    return 0;
+}
